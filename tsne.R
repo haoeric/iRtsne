3ac14263b78543f394b5a49fc675ec6ede0e5ddb
chen hao <haoeric@hotmail.com>
2015-08-12 18:27:29 +0800

    finish p calculation updates

--- tsne.R
+++ tsne.R
@@ -1,14 +1,18 @@
 tsne <- function(X, pca = TRUE, dims=2, initial_dims=NULL, perplexity=30, 
                  max_iter = 1000, min_cost=0, epoch_callback=NULL,whiten=TRUE, epoch=100 ){
     
+    if (is.null(initial_dims) | missing(initial_dims)) 
+        initial_dims <- ncol(X)
     ## initialize variables
+    n = nrow(X)
     momentum = 0.5
     final_momentum = 0.8
     mom_switch_iter = 250
     epsilon = 500
     min_gain = 0.01
     initial_P_gain = 4
-    eps = 2^(-52)                  # typical machine precision
+    eps = 2^(-52)                       # typical machine precision
+    ydata = matrix(rnorm(dims * n),n)   # initialize map results data
     
     ## Apply PCA
     X = as.matrix(X)
@@ -17,25 +21,16 @@ tsne <- function(X, pca = TRUE, dims=2, initial_dims=NULL, perplexity=30,
         X <- pca_result$x[,1:min(initial_dims,ncol(pca_result$x))]
     }
     
-    ## initialize map data
-    if (is.null(initial_dims) | missing(initial_dims)) 
-        initial_dims <- ncol(X)
-    n = nrow(X)
-	ydata = matrix(rnorm(dims * n),n)
-	
 	## P calculation
 	P = .x2p(X, perplexity, 1e-5)$P
-	P = .5 * (P + t(P))
-	P[P < eps]<-eps
-	P = P/sum(P)
-	
+	P = (P + t(P)) / (2*n)
+	P[P < eps] <- eps
 	
 	P = P * initial_P_gain
 	grads =  matrix(0,nrow(ydata),ncol(ydata))
 	incs =  matrix(0,nrow(ydata),ncol(ydata))
 	gains = matrix(1,nrow(ydata),ncol(ydata))
 
-	
 	for (iter in 1:max_iter){
 		if (iter %% epoch == 0) { # epoch
 			cost =  sum(apply(P * log((P+eps)/(Q+eps)),1,sum))
@@ -44,9 +39,8 @@ tsne <- function(X, pca = TRUE, dims=2, initial_dims=NULL, perplexity=30,
 			if (!is.null(epoch_callback)) epoch_callback(ydata)
 		}
 
-
 		sum_ydata = apply(ydata^2, 1, sum)
-		num =  1/(1 + sum_ydata +    sweep(-2 * ydata %*% t(ydata),2, -t(sum_ydata))) 
+		num =  1/(1 + sum_ydata + sweep(-2 * ydata %*% t(ydata),2, -t(sum_ydata))) 
 		diag(num)=0
 		Q = num / sum(num)
 		if (any(is.nan(num))) message ('NaN in grad. descent')
@@ -72,7 +66,7 @@ tsne <- function(X, pca = TRUE, dims=2, initial_dims=NULL, perplexity=30,
 
 
 
-
+## calculate entropy H and probability P with beta
 .Hbeta <- function(D, beta){
 	P = exp(-D^2 * beta)
 	sumP = sum(P)
@@ -89,25 +83,33 @@ tsne <- function(X, pca = TRUE, dims=2, initial_dims=NULL, perplexity=30,
 	r
 }
 
+
 .x2p <- function(X, perplexity = 30, tol = 1e-5){
 
 	n = nrow(X)
 	D = as.matrix(dist(X))
 	P = matrix(0, n, n )		
 	beta = rep(1, n)           # beta = 0.5/sigma^2
+	## test with variate perplexity
+	if(length(perplexity) < n)
+	    perplexity <- rep(perplexity, n)
 	logU = log2(perplexity)    # logU equal to entropy H
 	
 	for (i in 1:n){
+		Di = D[i, -i]
 		betamin = -Inf
 		betamax = Inf
-		Di = D[i, -i]
-		hbeta = .Hbeta(Di, beta[i])
-		H = hbeta$H
-		thisP = hbeta$P
-		Hdiff = H - logU
 		tries = 0
+		
+		while(tries < 1000){
+		    hbeta = .Hbeta(Di, ifelse(i>1, beta[i-1], beta[i]))
+		    H = hbeta$H
+		    thisP = hbeta$P
+		    Hdiff = H - logU[i]
+		    
+		    if (abs(Hdiff) <= tol)
+		        break
 
-		while(abs(Hdiff) > tol && tries < 50){
 			if (Hdiff > 0){
 				betamin = beta[i]
 				if (is.infinite(betamax)) beta[i] = beta[i] * 2
@@ -118,24 +120,27 @@ tsne <- function(X, pca = TRUE, dims=2, initial_dims=NULL, perplexity=30,
 				else beta[i] = ( beta[i] + betamin) / 2
 			}
 			
-			hbeta = .Hbeta(Di, beta[i])
-			H = hbeta$H
-			thisP = hbeta$P
-			Hdiff = H - logU
 			tries = tries + 1
-		}	
-			P[i,-i]  = thisP	
+		}
+		
+		P[i,-i]  = thisP	
 	}	
 	
-	r = {}
-	r$P = P
-	r$beta = beta
-	
-	sigma = sqrt(0.5/beta)
-	message('sigma summary: ', paste(names(summary(sigma)),':',summary(sigma),'|',collapse=''))
+	r <- list()
+	r$P <- P
+	r$sigma <- sqrt(0.5/beta)
 
 	r 
 }
+
+
+
+
+
+
+
+
+
 
 .whiten <- function(X, row.norm=FALSE, verbose=FALSE, n.comp=ncol(X))
 {  
